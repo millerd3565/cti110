5b5c85a69fc1adde0d3e744fc347b55656905d08
millerd3565 <47574908+millerd3565@users.noreply.github.com>
2019-02-12 14:36:23 -0500

    Add files via upload
    First submission.

---
+++ P2T1_SalesPrediction_DeShawnmiller.py
@@ -0,0 +1,18 @@
+# This project calculates and displays an amount
+# 2/12/19
+# CTI-110 P2T1 - Sales Prediction
+# DeShawn Miller
+
+
+# Get the projected total sales
+
+total_sales = float(input("Enter the projected total sales: "))
+
+# Calculate the profit as 23 percent of the total sales
+
+profit = total_sales * 0.23
+
+# Display the profit
+
+print("The profit $", format(profit, ',.2f'))
+    
